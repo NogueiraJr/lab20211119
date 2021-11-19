5f32e7f11535f82c41c7ff6118fbceebca370091
Nogueira <anogueirajr@gmail.com>
2021-11-19 10:28:37 -0300

    MUI CssBaseline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       />
     </Head>
     <Component {...pageProps} />
+    <CssBaseline />
   </>);
 }
 
